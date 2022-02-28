54f7976dcd0fb89b065e2273eff7d674f0523100
Owen Coogan <37589993+OwenCoogan@users.noreply.github.com>
2022-02-28 22:23:06 +0100

    adding stuff

---
+++ src/components/Episodes/EpisodeCard.vue
@@ -0,0 +1,12 @@
+<template>
+      <article>
+        <h2>Episode Name : {{title}}</h2>
+        <img :src="image"/>
+      </article>
+</template>
+<script>
+export default {
+  props: ['title','image','id'],
+}
+</script>
+
---
+++ src/components/Episodes/EpisodeList.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+      <div v-for="episode in episodes" :key="episode.name" class="w-1/6">
+      <EpisodeCard
+      :title='episode.name'
+      :image='episode.image?.medium'
+      :id='episode.id'
+      />
+    </div>
+
+  </section>
+</template>
+<script>
+import  EpisodeCard from '../Episodes/EpisodeCard.vue'
+export default {
+  props:{
+    episodes: Array
+  },
+  components:{
+    EpisodeCard
+  },
+}
+</script>
+
