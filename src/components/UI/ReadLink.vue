ba6ecee5e4984c639c41d687cc6f4ea859c7a9a8
Owen Coogan <37589993+OwenCoogan@users.noreply.github.com>
2022-03-12 16:20:25 +0100

    remove header from views and into app.vue

--- src/components/UI/ReadLink.vue
+++ src/components/UI/ReadLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <a class="bg-white hover:bg-slate-700 bg-opacity-75 text-black font-bold py-2 px-4 rounded m-1" :href="`https://www.youtube.com/results?search_query=${title}`">
+  <a class="bg-white text-lg hover:bg-slate-700 bg-opacity-75 text-black font-bold py-2 px-4 rounded m-1" :href="`https://www.youtube.com/results?search_query=${title}`">
     Lecture
   </a>
 </template>
